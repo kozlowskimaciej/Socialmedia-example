aafee909c4fbcea361366ad653cfedc152767d4f
Kozprog <maciejmaciejkoz@gmail.com>
2021-08-26 16:38:11 +0200

    geolocation

--- frontend/assets/script.js
+++ frontend/assets/script.js
@@ -11,6 +11,27 @@ var posts = [];
 
 moment.locale(language);
 
+var options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+
+function success(pos) {
+    var crd = pos.coords;
+  
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+};
+
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
 function checkForBottom(){
     setInterval( ()=>{if(window.innerHeight+window.scrollY>document.body.clientHeight-50)GetPosts();}, 1000);
 }
